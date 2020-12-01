39ff8d38b69b5636fd9db030e2aa883afeb17883
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-12-01 16:17:32 -0500

    add second object test - oops

--- src/name.test.js
+++ src/name.test.js
@@ -8,4 +8,10 @@ describe('getName function', () => {
 
         expect(name).toEqual('spot')
     })
+    it('returns the name property of an object', () => {
+        const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
+        const name = getName(character);
+
+        expect(name).toEqual('Aang')
+    })
 })
