92007c214f79b237061a18772cb0b7a1ac789f41
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-12-01 16:18:56 -0500

    refactor and destructure getName

--- src/name.test.js
+++ src/name.test.js
@@ -10,6 +10,7 @@ describe('getName function', () => {
     })
     it('returns the name property of an object', () => {
         const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
+        
         const name = getName(character);
 
         expect(name).toEqual('Aang')
