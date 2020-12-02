6d25337c72f835a016ffe44612626950a950c554
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-12-01 19:43:44 -0500

    final

--- src/fetch.test.js
+++ src/fetch.test.js
@@ -1,14 +1,21 @@
 const { fetchQuotes } = require('./fetch');
+const request = require('superagent');
+
+jest.mock('superagent');
 
 describe('fetchQuotes function', () => {
     it('returns a single quote with specific format from Futurama Quote API', async() => {
-        const quote = {
-            name: 'Bender',
-            text: 'I\'m a fraud. A poor, lazy, sexy fraud.',
-            image: 'https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png'
-        }
+        request.get.mockResolvedValue({
+            body: [
+                {
+                    character: 'Bender',
+                    quote: 'I\'m a fraud. A poor, lazy, sexy fraud.',
+                    image: 'https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png'
+                }
+            ]
+        });
 
-        const result = await fetchQuotes(quote);
+        const result = await fetchQuotes();
 
         expect(result).toEqual({
             name: 'Bender',
